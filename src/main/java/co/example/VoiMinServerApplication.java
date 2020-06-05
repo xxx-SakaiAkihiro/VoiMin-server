fc8ae49adcde883e51c0e9187e8928e4bb8d51af
SakaiAkihiro <akihiro.sakai@rakus-partners.co.jp>
2020-06-05 16:50:55 +0900

    first commit

---
+++ src/main/java/co/example/VoiMinServerApplication.java
@@ -0,0 +1,13 @@
+package co.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoiMinServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoiMinServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/example/VoiMinServerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoiMinServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
